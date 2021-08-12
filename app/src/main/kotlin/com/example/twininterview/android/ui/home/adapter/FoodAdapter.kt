6df3fc84c2a9826420cfdb3da3a296be9fd02ad8
Vishal <vishal@flick2know.com>
2021-08-12 07:00:43 +0530

    Implement FoodAdapter

--- app/src/main/kotlin/com/example/twininterview/android/ui/home/adapter/FoodAdapter.kt
+++ app/src/main/kotlin/com/example/twininterview/android/ui/home/adapter/FoodAdapter.kt
@@ -6,22 +6,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.twininterview.android.adapter.viewholder.DefaultHolder
 import com.example.twininterview.android.data.domain.food.model.Food
 import com.example.twininterview.android.databinding.ListItemFoodHomeBinding
+import com.example.twininterview.android.extensions.toggleView
+import com.example.twininterview.android.extensions.loadImage
 
 internal class FoodAdapter(
-    val foodItems: List<Food>
+    var foodItems: List<Food>
 ) : RecyclerView.Adapter<DefaultHolder<ListItemFoodHomeBinding>>() {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DefaultHolder<ListItemFoodHomeBinding> {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DefaultHolder<ListItemFoodHomeBinding> {
         val inflater = LayoutInflater.from(parent.context)
         return DefaultHolder(ListItemFoodHomeBinding.inflate(inflater, parent, false))
     }
 
     override fun onBindViewHolder(holder: DefaultHolder<ListItemFoodHomeBinding>, position: Int) {
-
+        holder.binding.apply {
+            model = foodItems[position]
+            toggleSelected = toggleView { isSelected = !it }
+            isSelected = false
+        }
     }
 
     override fun getItemCount(): Int {
         return foodItems.size
+    }
+
+    fun update(foodItems: List<Food>) {
+        this.foodItems = foodItems
+        notifyDataSetChanged()
     }
 }
