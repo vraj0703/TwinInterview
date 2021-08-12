4ad3f8b1752c5ec80564cbffe15743c5ad0ebd25
Vishal <vishal@flick2know.com>
2021-08-12 07:00:13 +0530

    Add SerializedName in food model

--- app/src/main/kotlin/com/example/twininterview/android/data/domain/food/model/Food.kt
+++ app/src/main/kotlin/com/example/twininterview/android/data/domain/food/model/Food.kt
@@ -1,9 +1,20 @@
 package com.example.twininterview.android.data.domain.food.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Food (
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("title")
     val title: String,
+
+    @SerializedName("image_url")
     val imageUrl: String,
+
+    @SerializedName("is_recent")
     val isRecent: Boolean,
+
+    @SerializedName("is_tfy")
     val isTfy: Boolean
 )
